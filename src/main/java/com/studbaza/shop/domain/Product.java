d82e15c6133327c307950b64907030d72aa46e60
admly <admly@interia.pl>
2017-11-22 16:04:34 +0100

    Added product model and jackson library

---
+++ src/main/java/com/studbaza/shop/domain/Product.java
@@ -0,0 +1,152 @@
+package com.studbaza.shop.domain;
+
+
+import java.math.BigDecimal;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlTransient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.web.multipart.MultipartFile;
+
+
+public class Product {
+    @Pattern(regexp = "P[0-9]+", message = "{Pattern.Product.productId.validation}")
+    private String productId;
+
+    @Size(min = 4, max = 50, message = "{Size.Product.name.validation}")
+    private String name;
+
+    @Min(value = 0, message = "Min.Product.unitPrice.validation}")
+    @Digits(integer = 8, fraction = 2, message = "{Digits.Product.unitPrice.validation}")
+    @NotNull(message = "{NotNull.Product.unitPrice.validation}")
+    private BigDecimal unitPrice;
+
+    private String description;
+    private String manufacturer;
+    private String category;
+    private long unitsInStock;
+    private long unitsInOrder;
+    private MultipartFile productImage;
+
+    public Product() {
+        super();
+    }
+
+    public Product(String productId, String name, BigDecimal unitPrice) {
+        this.productId = productId;
+        this.name = name;
+        this.unitPrice = unitPrice;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public long getUnitsInStock() {
+        return unitsInStock;
+    }
+
+    public void setUnitsInStock(long unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public long getUnitsInOrder() {
+        return unitsInOrder;
+    }
+
+    public void setUnitsInOrder(long unitsInOrder) {
+        this.unitsInOrder = unitsInOrder;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        if (productId == null) {
+            if (other.productId != null)
+                return false;
+        } else if (!productId.equals(other.productId))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((productId == null) ? 0 :
+                productId.hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Produkt [productId=" + productId + ", nazwa=" + name + "]";
+    }
+
+    @XmlTransient
+    public MultipartFile getProductImage() {
+        return productImage;
+    }
+
+    @JsonIgnore
+    public void setProductImage(MultipartFile productImage) {
+        this.productImage = productImage;
+    }
+
+
+}
