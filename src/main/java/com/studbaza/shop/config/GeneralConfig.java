88b5e5829f5aafab005467fa9fb7456dde8567f5
Artur Krotiuk <arturkrotiuk@gmail.com>
2017-12-05 22:52:45 +0100

    great fix

--- src/main/java/com/studbaza/shop/config/GeneralConfig.java
+++ src/main/java/com/studbaza/shop/config/GeneralConfig.java
@@ -3,6 +3,7 @@ package com.studbaza.shop.config;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+
 public class GeneralConfig {
     //To do config
 }
--- src/main/java/com/studbaza/shop/domain/ProductRepository.java
+++ src/main/java/com/studbaza/shop/domain/ProductRepository.java
@@ -3,5 +3,6 @@ package com.studbaza.shop.domain;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-public interface ProductRepository {
+@Repository
+public interface ProductRepository extends CrudRepository<Product, String> {
 }
